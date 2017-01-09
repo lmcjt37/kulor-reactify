cdce81c5e73fbb6a8dd74e175915d2b84d8fa146
lmcjt37 <lmcjt@outlook.com>
2017-01-09 15:34:14 +0000

    :poop: removed node modules

--- node_modules/react-toolbox/components/app_bar/index.js
+++
@@ -1,9 +0,0 @@
-import { themr } from 'react-css-themr';
-import { AppBar } from './AppBar.js';
-import { APP_BAR } from '../identifiers.js';
-import theme from './theme.scss';
-
-const ThemedAppBar = themr(APP_BAR, theme)(AppBar);
-
-export default ThemedAppBar;
-export { ThemedAppBar as AppBar };
