b7fe23fe1ff0a5307e94343dcb24eb9243c8f6f9
Luke Taylor <lmcjt@outlook.com>
2017-01-24 00:23:30 +0000

    :construction: RGB field still needs work

--- src/app/helper/colourHelper.js
+++ src/app/helper/colourHelper.js
@@ -109,12 +109,15 @@ var helper = {
         if (tmp.substring(0, 1) === " ") {
             tmp = tmp.substring(1);
         }
-        console.log("tmp : ", tmp);
-        if (tmp.length >= 10) {
-            return tmp.substring(0, 9);
-        } else {
+        /*
+        * Needs to validate and set max on rgb value
+        **/
+        
+        // if (tmp.length >= 10) {
+        //     return tmp.substring(0, 9);
+        // } else {
             return tmp;
-        }
+        // }
     },
 
     trimHex: function(colour) {
