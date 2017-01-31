849ea6fa71f70e89389e86cd16dfd2bc1c9050a0
lmcjt37 <lmcjt@outlook.com>
2017-01-31 15:13:49 +0000

    :construction: Fixes still to be made to get tests running

--- test/specs/app.spec.js
+++ test/specs/app.spec.js
@@ -2,18 +2,14 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
-// import Foo from '../src/Foo';
+import App from '../src/app/App';
 
-describe("A spec for app.js", function() {
-  // it("contains spec with an expectation", function() {
-  //   expect(shallow(<Foo />).contains(<div className="foo" />)).to.equal(true);
-  // });
-  //
-  // it("contains spec with an expectation", function() {
-  //   expect(shallow(<Foo />).is('.foo')).to.equal(true);
-  // });
-  //
-  // it("contains spec with an expectation", function() {
-  //   expect(mount(<Foo />).find('.foo').length).to.equal(1);
-  // });
+describe("Tests for app.js", function() {
+
+  it("checks correct setup of initial page", function() {
+      expect(shallow(<App />).contains(<Header />)).to.equal(true);
+      expect(shallow(<App />).contains(<Inputs />)).to.equal(true);
+      expect(shallow(<App />).contains(<Sliders />)).to.equal(true);
+  });
+
 });
