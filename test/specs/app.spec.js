fb934fad2422cb2f3cb3930388746aa3bc23ca93
lmcjt37 <lmcjt@outlook.com>
2017-01-31 14:43:04 +0000

    :tada: Initial spec - wip

---
+++ test/specs/app.spec.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+// import Foo from '../src/Foo';
+
+describe("A spec for app.js", function() {
+  // it("contains spec with an expectation", function() {
+  //   expect(shallow(<Foo />).contains(<div className="foo" />)).to.equal(true);
+  // });
+  //
+  // it("contains spec with an expectation", function() {
+  //   expect(shallow(<Foo />).is('.foo')).to.equal(true);
+  // });
+  //
+  // it("contains spec with an expectation", function() {
+  //   expect(mount(<Foo />).find('.foo').length).to.equal(1);
+  // });
+});
