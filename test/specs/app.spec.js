9d021f7be447b98dd77dda2c0d16a798307c7985
lmcjt37 <lmcjt@outlook.com>
2017-02-01 09:32:04 +0000

    :construction: adding mocking and stubs

--- test/specs/app.spec.js
+++ test/specs/app.spec.js
@@ -1,20 +1,51 @@
 import React from 'react';
-import {
-	findRenderedDOMComponentWithTag
-} from 'react-addons-test-utils';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
+import sinon from 'sinon';
+import proxyquire from 'proxyquire';
+proxyquire.noCallThru();
 
-import App from '../../src/app/App.js';
+const MainSpy = sinon.spy(),
+    ColourHelperSpy = sinon.spy(),
+    InputsSpy = sinon.spy(),
+    SlidersSpy = sinon.spy(),
+    HeaderSpy = sinon.spy(),
+    configSpy = sinon.stub().returns({anchor: {}, image: {}});
+
+const App = proxyquire('../../src/app/App.js', {
+    './theme/main.scss': MainSpy,
+    './helper/colourHelper.js': ColourHelperSpy,
+    './containers/Input.js': InputsSpy,
+    './containers/Slider.js': SlidersSpy,
+    './containers/Header.js': HeaderSpy,
+    './config': configSpy
+}).default;
 
 describe("Tests for app.js", function() {
 
-    it("checks correct setup of initial page", function() {
-        const component = shallow(<App />);
+    beforeEach(function () {
+        this.state = {
+            rgb: '91,50,86',
+            hex: '5B3256',
+            hue: 0,
+            rgbOpacity: 1,
+            hexOpacity: 1,
+            saturation: 0,
+            lightness: 0,
+            type: '',
+            theme: 'light',
+            bgColour: '5B3256'
+        };
+    });
 
-        expect(findRenderedDOMComponentWithTag(component, 'Header')).to.be.ok;
-        expect(findRenderedDOMComponentWithTag(component, 'Inputs')).to.be.ok;
-        expect(findRenderedDOMComponentWithTag(component, 'Sliders')).to.be.ok;
+    it("checks correct setup of initial page", function() {
+        const wrapper = shallow(<App />);
+
+        expect(wrapper.type()).to.equal('div');
+
+        console.log(wrapper.prop('style'));
+
+        expect(wrapper.prop('style')).to.equal({ backgroundColor: '#5B3256' });
 
     });
 
