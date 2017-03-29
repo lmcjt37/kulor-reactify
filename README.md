f8e094b6ebcefdcdbeb64688c01f035b77d52894
lmcjt37 <lmcjt@outlook.com>
2017-03-29 14:46:17 +0100

    :books: Version increment and added information - issue #30

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Includes:
 * Randomisation
 * Lighten
 * Darken
+* Handles alpha and conversion
 * Handy Help Hints
 * More to come...
 
@@ -27,7 +28,6 @@ Working project: http://lmcjt.com/kulor
 - Github corner link was created by [Tim Holman](http://tholman.com/github-corners/)
 
 ### Pipeline
-- Add alpha
 - lighten/darken percentage +/-
 - Colour palette for storing colours from the session
 - Ability to get different colour combos (triad/complementary/tetrad)
--- src/app/config/dialogs.js
+++ src/app/config/dialogs.js
@@ -7,7 +7,8 @@ const dialogs = {
             "HEX input is a six digit value where every 2 digits represents red, green and blue. Values for each colour can range from 0(black)-9...A-F(White). Examples; FF6600, #1199FF",
             "HUE, SATURATION, and LIGHTNESS represent the HSL colour mode. Hue accepts a value of 0-360, Saturation and Lightness accept a value of 0-100.",
             "Random allows you to quickly find new colours, some good, some bad, some ugly.",
-            "Lighten/Darken, lighten and darken the current colour by 10%."
+            "Lighten/Darken, lighten and darken the current colour by 10%.",
+            "Alpha allows you to alter your colour with opacity to two decimal places"
         ]
     }
 }
