bcb4983e6502be603467aefa8244398380c7d01d
Luke Taylor <lmcjt@outlook.com>
2017-04-05 10:07:50 +0100

    :wrench: Fixed some environment issues and added additional test for initial state object

--- test/specs/app.spec.js
+++ test/specs/app.spec.js
@@ -1,5 +1,6 @@
+/* Remember: https://mochajs.org/#arrow-functions*/
 import React from 'react';
-import { mount, render, shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import sinon from 'sinon';
 
 import App from '../../src/app/App';
@@ -8,13 +9,44 @@ describe("Tests for app.js",() => {
 
     beforeEach(() => {});
 
-    it("checks correct setup of initial page", () => {
-        
-        const wrapper = shallow(<App />);
+    afterEach(() => {});
 
-        expect(wrapper.type()).to.equal('div');
+    describe("and we check initial", () => {
 
-        expect(wrapper.prop('style')).to.deep.equal({ backgroundColor: '#5b3256' });
+        it("state object", () => {
+
+            const wrapper = mount(<App />);
+
+            expect(wrapper.node.state).to.deep.equal({
+                rgb: '91,50,86',
+                hex: '5b3256',
+                hue: 307,
+                rgbOpacity: 1,
+                hexOpacity: 1,
+                saturation: 29,
+                lightness: 28,
+                alpha: 1,
+                type: '',
+                theme: 'light',
+                bgColour: '5b3256',
+                isOpen: false,
+                isHandheld: false,
+                isDialogActive: false,
+                showToast: false,
+                toastMessage: ''
+            });
+
+        });
+
+        it("root element", () => {
+
+            const wrapper = shallow(<App />);
+
+            expect(wrapper.type()).to.equal('div');
+
+            expect(wrapper.prop('style')).to.deep.equal({ backgroundColor: '#5b3256' });
+
+        });
 
     });
 
