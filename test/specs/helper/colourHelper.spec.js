32a0c17da5e828c53a4d1f5a64edaae217237988
Luke Taylor <lmcjt@outlook.com>
2017-05-08 10:28:34 +0100

    :construction: Added more tests around methods randomise, lighten, darken

--- test/specs/helper/colourHelper.spec.js
+++ test/specs/helper/colourHelper.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import sinon from 'sinon';
-
 import ColourHelper from "helper/colourHelper";
 
 describe("Tests for helper/colourHelper.js", () => {
@@ -67,44 +66,65 @@ describe("Tests for helper/colourHelper.js", () => {
 
     });
 
-    // describe("and getScreenSize, when", () => {
-    //
-    //     beforeEach(() => {
-    //
-    //         sinon.stub(UtilsHelper, 'getScreenWidth');
-    //
-    //     });
-    //
-    //     afterEach(() => {
-    //
-    //         UtilsHelper.getScreenWidth.restore();
-    //
-    //     });
-    //
-    //     it("width > 768", () => {
-    //
-    //         UtilsHelper.getScreenWidth.returns(800);
-    //
-    //         expect(UtilsHelper.getScreenSize()).to.deep.equal({ isHandheld: false, rgbOpacity: 1, hexOpacity: 1 });
-    //
-    //     });
-    //
-    //     it("width < 768 && width > 481", () => {
-    //
-    //         UtilsHelper.getScreenWidth.returns(600);
-    //
-    //         expect(UtilsHelper.getScreenSize()).to.deep.equal({ isHandheld: true, rgbOpacity: 1, hexOpacity: 1 });
-    //
-    //     });
-    //
-    //     it("width < 768 && width < 481", () => {
-    //
-    //         UtilsHelper.getScreenWidth.returns(400);
-    //
-    //         expect(UtilsHelper.getScreenSize()).to.deep.equal({ isHandheld: true, rgbOpacity: 0, hexOpacity: 0 });
-    //
-    //     });
-    //
-    // });
+    describe("and handles button functions when clicked, for", () => {
+
+        it("randomise", () => {
+
+            let spy = sinon.spy();
+            spy(ColourHelper.randomise());
+
+            sinon.assert.calledWith(spy, sinon.match.has("rgb", sinon.match.string)
+                .and(sinon.match.has("hex", sinon.match.string))
+                .and(sinon.match.has("hue", sinon.match.number))
+                .and(sinon.match.has("rgbOpacity", sinon.match.number))
+                .and(sinon.match.has("hexOpacity", sinon.match.number))
+                .and(sinon.match.has("saturation", sinon.match.number))
+                .and(sinon.match.has("lightness", sinon.match.number))
+                .and(sinon.match.has("alpha", sinon.match.number))
+                .and(sinon.match.has("theme", sinon.match.string))
+                .and(sinon.match.has("bgColour", sinon.match.string))
+            );
+
+        });
+
+        it("lighten", () => {
+
+            let spy = sinon.spy();
+            spy(ColourHelper.lighten("5b3256"));
+
+            sinon.assert.calledWith(spy, sinon.match.has("rgb", "124,68,117")
+                .and(sinon.match.has("hex", "7c4475"))
+                .and(sinon.match.has("hue", 307))
+                .and(sinon.match.has("rgbOpacity", sinon.match.number))
+                .and(sinon.match.has("hexOpacity", sinon.match.number))
+                .and(sinon.match.has("saturation", 29))
+                .and(sinon.match.has("lightness", 38))
+                .and(sinon.match.has("alpha", sinon.match.number))
+                .and(sinon.match.has("theme", "light"))
+                .and(sinon.match.has("bgColour", "124,68,117,1.0"))
+            );
+
+        });
+
+        it("darken", () => {
+
+            let spy = sinon.spy();
+            spy(ColourHelper.darken("5b3256"));
+
+            sinon.assert.calledWith(spy, sinon.match.has("rgb", "58,32,55")
+                .and(sinon.match.has("hex", "3a2037"))
+                .and(sinon.match.has("hue", 307))
+                .and(sinon.match.has("rgbOpacity", sinon.match.number))
+                .and(sinon.match.has("hexOpacity", sinon.match.number))
+                .and(sinon.match.has("saturation", 29))
+                .and(sinon.match.has("lightness", 18))
+                .and(sinon.match.has("alpha", sinon.match.number))
+                .and(sinon.match.has("theme", "light"))
+                .and(sinon.match.has("bgColour", "58,32,55,1.0"))
+            );
+
+        });
+
+    });
 
 });
